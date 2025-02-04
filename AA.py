bf1c7f0e2bae4ab822f5da65436a58b39fb5383f
i-neeed-sleep <kammyl@nycstudents.net>
2025-02-04 09:25:12 -0500

    DATA

---
+++ AA.py
@@ -0,0 +1,17 @@
+#EEE
+
+sentence = input("Input a sentence. ")
+eee = sentence.split()
+
+oo = 0
+
+for i in eee:
+    oo = oo + 1
+
+print(f"There are {oo} words")
+
+numm = input("Give a number in number form")
+numm = int(numm)
+
+if numm 
+
