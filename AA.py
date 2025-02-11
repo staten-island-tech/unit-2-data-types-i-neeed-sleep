dcedc0084d7ff679118a0bc71dc2022e490d7715
i-neeed-sleep <kammyl@nycstudents.net>
2025-02-11 09:25:38 -0500

    eeeeeeeeeee

--- AA.py
+++ AA.py
@@ -50,3 +50,37 @@ def fac(x):
             return(fac)
 
 fac(nnn)
+
+
+#GCF
+frt = input("Type number")
+scd = input("Type another number")
+
+frt = int(frt)
+scd = int(scd)
+
+fct = []
+fctB = []
+
+def ftr(y):
+    for i in range(1, y + 1):
+        if y % i == 0:
+            fct.append(i)
+
+def ftrB(y):
+    for i in range(1, y + 1):
+        if y % i == 0:
+            fctB.append(i)
+
+ftr(frt)
+ftrB(scd)
+
+cf = []
+
+for item in fct:
+    if item in fctB:
+        cf.append(item)
+
+gcf = max(cf)
+
+print(f"The greatest common factor between {frt} and {scd} is {gcf}")
---
+++ MadLibs.py
@@ -0,0 +1,11 @@
+vrb = input("Give me a present tense verbs")
+pstV = input("Give me a past tense verb")
+nn = input("Now give me 2 animals or mythical creatures")
+num = input("Tell me a number")
+celeb = input("Name a celebrity")
+
+nn = nn.split()
+nnA, nnB = nn
+
+
+print(f"{celeb} was {vrb}, until the {nnA} {pstV}.")
