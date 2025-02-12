43fcae72f3cf2d9d840ab2676b32ecaf894f97bd
i-neeed-sleep <kammyl@nycstudents.net>
2025-02-12 09:04:44 -0500

    unit2

--- MadLibs.py
+++ MadLibs.py
@@ -1,11 +1,18 @@
-vrb = input("Give me a present tense verbs")
-pstV = input("Give me a past tense verb")
+vrb = input("Give me a present tense verb (-ing ending)")
+pstV = input("Give me 2 past tense verbs (preferably both ending in -ed)")
 nn = input("Now give me 2 animals or mythical creatures")
+obj = input("Name an object")
 num = input("Tell me a number")
 celeb = input("Name a celebrity")
 
+
 nn = nn.split()
-nnA, nnB = nn
+nnA = nn[0]
+nnB = nn[1]
+pstV = pstV.split()
+pstVa, pstVb = pstV
+celeb = celeb.capitalize()
+obj = obj.capitalize()
 
 
-print(f"{celeb} was {vrb}, until the {nnA} {pstV}.")
+print(f"{celeb} was {vrb}, until the {nnA} {pstVa}. {obj}s {pstVb}. {celeb}'s {num} {nnB}s also {pstVb}. Everything was chaos.")
